404d90d449c709621ee2d101d360bdce0ea7f8e4
Josh Scoggins <jscoggins@parasoft.com>
2018-01-15 11:33:11 -0800

    Added word reading abilities

--- forth.cc
+++ forth.cc
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 
-
+std::string readWord(std::istream& input) {
+    std::string word;
+    input >> word;
+    return word;
+}
 int main() {
+    auto execute = true;
+    while (execute) {
+        
+    }
     return 0;
 }

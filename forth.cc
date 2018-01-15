d98c0b7c79e62412294b28b9150445a55f0be9ef
Josh Scoggins <jscoggins@parasoft.com>
2018-01-15 11:39:33 -0800

    Added a basic concept of a "dictionary entry"

--- forth.cc
+++ forth.cc
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <string>
 
+class DictionaryEntry {
+    public:
+        DictionaryEntry() = default;
+        ~DictionaryEntry() = default;
+};
 std::string readWord(std::istream& input) {
     std::string word;
     input >> word;
     return word;
 }
+DictionaryEntry& getNullEntry() noexcept {
+    static DictionaryEntry nullEntry;
+    return nullEntry;
+}
+DictionaryEntry& lookupWord(const std::string& word) noexcept {
+    return getNullEntry();
+}
+
 int main() {
-    auto execute = true;
-    while (execute) {
+    while (true) {
         auto result = readWord(std::cin);
         if (result == "quit") {
-            execute = false;
+            break;
         } else {
+            auto& entry = lookupWord(result);
+            if (&entry == &getNullEntry()) {
+                std::cout << "Unknown word: " << result << std::endl;
+                continue;
+            }
             std::cout << "word: " << result << std::endl;
         }
     }

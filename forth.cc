3716dc1ac008b2bddb01060e38ee2a44e09ae402
Josh Scoggins <jscoggins@parasoft.com>
2018-01-15 11:47:07 -0800

    Define Dictionary and ParameterStack

--- forth.cc
+++ forth.cc
@@ -1,32 +1,58 @@
 #include <iostream>
 #include <string>
+#include <stack>
+#include <list>
 
 class DictionaryEntry {
     public:
         DictionaryEntry() = default;
+        DictionaryEntry(const std::string& name);
         ~DictionaryEntry() = default;
+        const std::string& getName() const noexcept { return _name; }
+    private:
+        std::string _name;
 };
+
+class Dictionary : public std::list<DictionaryEntry> {
+    public:
+        using Parent = std::list<DictionaryEntry>;
+        using Self = Dictionary;
+        static const DictionaryEntry& getNullEntry() noexcept {
+            static DictionaryEntry nullEntry;
+            return nullEntry;
+        }
+        static bool isNullEntry(const DictionaryEntry& entry) noexcept {
+            return &entry == &(getNullEntry());
+        }
+    public:
+        using Parent::Parent;
+        const DictionaryEntry& lookupWord(const std::string& word) noexcept;
+};
+
+template<typename T = int>
+using ParameterStack = std::stack<T, std::list<T>>;
+
+DictionaryEntry::DictionaryEntry(const std::string& name) : _name(name) { }
+
 std::string readWord(std::istream& input) {
     std::string word;
     input >> word;
     return word;
 }
-DictionaryEntry& getNullEntry() noexcept {
-    static DictionaryEntry nullEntry;
-    return nullEntry;
-}
-DictionaryEntry& lookupWord(const std::string& word) noexcept {
+const DictionaryEntry& Dictionary::lookupWord(const std::string& word) noexcept {
     return getNullEntry();
 }
 
+
 int main() {
+    Dictionary dict;
     while (true) {
         auto result = readWord(std::cin);
         if (result == "quit") {
             break;
         } else {
-            auto& entry = lookupWord(result);
-            if (&entry == &getNullEntry()) {
+            auto& entry = dict.lookupWord(result);
+            if (Dictionary::isNullEntry(entry)) {
                 std::cout << "Unknown word: " << result << std::endl;
                 continue;
             }

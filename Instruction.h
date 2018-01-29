adf2402d2cd135165ba516cecab37ef1a59e4b02
Josh Scoggins <jscoggins@parasoft.com>
2018-01-29 13:59:58 -0800

    Added instruction operation lookup

--- Instruction.h
+++ Instruction.h
@@ -6,10 +6,44 @@
 namespace forth {
 union Instruction {
     Instruction(Address v) : _value(v) { };
+    Instruction(const Instruction& other) : _value(other._value) { };
+    ~Instruction() { _value = 0; };
     Address _value;
     byte backingStore[sizeof(Address)];
     QuarterAddress quads[sizeof(Address) / sizeof(QuarterAddress)];
     HalfAddress halves[sizeof(Address) / sizeof(HalfAddress)];
 };
+
+enum class Operation : byte {
+    Nop,
+    Combine,
+    Multiply,
+    Divide,
+    Modulo,
+    Not,
+    Minus,
+    And,
+    Or,
+    GreaterThan,
+    LessThan,
+    Xor,
+    Shift,
+    PopRegister,
+    PushRegister,
+    Equals,
+    TypeValue,
+    Load,
+    Store,
+    Pow,
+    Sin,
+    Cos,
+    Tan,
+    Atan,
+    Atan2,
+    Count,
+};
+
+static_assert(static_cast<byte>(-1) >= static_cast<byte>(Operation::Count), "Too many operations defined!");
+
 } // end namespace forth
 #endif // end INSTRUCTION_H__

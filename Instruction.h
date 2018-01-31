93585eaec42bf17dc05d0e023c2a2174d7bda3dd
Josh Scoggins <jscoggins@parasoft.com>
2018-01-31 15:14:57 -0800

    Added routines to encode full address words

--- Instruction.h
+++ Instruction.h
@@ -12,6 +12,7 @@ constexpr T encodeBits(T value, R newValue) noexcept {
     return (value & ~mask) | ((static_cast<T>(newValue) << shift) & mask);
 }
 
+
 union Molecule {
     Molecule(Address v) : _value(v) { };
     Molecule(const Molecule& other) : _value(other._value) { };
@@ -45,6 +46,18 @@ enum class TargetRegister : byte {
     RegisterTIP, // instruction pointer type
     Error,
 };
+constexpr byte encodeDestinationRegister(byte value, TargetRegister reg) noexcept {
+    return encodeBits<byte, byte, 0x0F, 0>(value, (byte)reg);
+}
+constexpr byte encodeDestinationRegister(TargetRegister reg) noexcept {
+    return encodeDestinationRegister(0, reg);
+}
+constexpr byte encodeSourceRegister(byte value, TargetRegister reg) noexcept {
+    return encodeBits<byte, byte, 0xF0, 4>(value, (byte)reg);
+}
+constexpr byte encodeRegisterPair(TargetRegister dest, TargetRegister src) noexcept {
+    return encodeSourceRegister(encodeDestinationRegister(dest), src);
+}
 static_assert(byte(TargetRegister::Error) <= 16, "Too many registers defined!");
 static constexpr bool involvesDiscriminantRegister(TargetRegister r) noexcept {
     switch (r) {
@@ -104,20 +117,56 @@ enum class Operation : byte {
 
 
 namespace Instruction {
-    constexpr byte add() noexcept { return static_cast<byte>(Operation::Add); }
-    constexpr byte sub() noexcept { return static_cast<byte>(Operation::Subtract); }
-    constexpr byte mul() noexcept { return static_cast<byte>(Operation::Multiply); }
-    constexpr byte div() noexcept { return static_cast<byte>(Operation::Divide); }
-    constexpr byte mod() noexcept { return static_cast<byte>(Operation::Modulo); }
-    constexpr byte notOp() noexcept { return static_cast<byte>(Operation::Not); }
-    constexpr byte minus() noexcept { return static_cast<byte>(Operation::Minus); }
-    constexpr byte andOp() noexcept { return static_cast<byte>(Operation::And); }
-    constexpr byte orOp() noexcept { return static_cast<byte>(Operation::Or); }
-    constexpr byte greaterThan() noexcept { return static_cast<byte>(Operation::GreaterThan); }
-    constexpr byte lessThan() noexcept { return static_cast<byte>(Operation::LessThan); }
-    constexpr byte xorOp() noexcept { return static_cast<byte>(Operation::Xor); }
-    constexpr byte shiftRight() noexcept { return static_cast<byte>(Operation::ShiftRight); }
-    constexpr byte shiftLeft() noexcept { return static_cast<byte>(Operation::ShiftLeft); }
+    constexpr byte singleByteOp(Operation op) noexcept { return static_cast<byte>(op); }
+    constexpr QuarterAddress encodeTwoByte(Operation first, byte second) noexcept {
+        return encodeBits<QuarterAddress, byte, 0xFF00, 8>(QuarterAddress(first), second);
+    }
+    constexpr QuarterAddress encodeTwoByte(Operation first, TargetRegister dest, TargetRegister src) noexcept {
+        return encodeTwoByte(first, encodeRegisterPair(dest, src));
+    }
+    constexpr byte add() noexcept { return singleByteOp(Operation::Add); }
+    constexpr byte sub() noexcept { return singleByteOp(Operation::Subtract); }
+    constexpr byte mul() noexcept { return singleByteOp(Operation::Multiply); }
+    constexpr byte div() noexcept { return singleByteOp(Operation::Divide); }
+    constexpr byte mod() noexcept { return singleByteOp(Operation::Modulo); }
+    constexpr byte notOp() noexcept { return singleByteOp(Operation::Not); }
+    constexpr byte minus() noexcept { return singleByteOp(Operation::Minus); }
+    constexpr byte andOp() noexcept { return singleByteOp(Operation::And); }
+    constexpr byte orOp() noexcept { return singleByteOp(Operation::Or); }
+    constexpr byte greaterThan() noexcept { return singleByteOp(Operation::GreaterThan); }
+    constexpr byte lessThan() noexcept { return singleByteOp(Operation::LessThan); }
+    constexpr byte xorOp() noexcept { return singleByteOp(Operation::Xor); }
+    constexpr byte shiftRight() noexcept { return singleByteOp(Operation::ShiftRight); }
+    constexpr byte shiftLeft() noexcept { return singleByteOp(Operation::ShiftLeft); }
+
+    constexpr QuarterAddress popRegister(TargetRegister destination) noexcept {
+        return encodeTwoByte(Operation::PopRegister, encodeDestinationRegister(destination));
+    }
+    constexpr QuarterAddress pushRegister(TargetRegister destination) noexcept {
+        return encodeTwoByte(Operation::PushRegister, encodeDestinationRegister(destination));
+    }
+    constexpr byte equals() noexcept { return singleByteOp(Operation::Equals); }
+    constexpr byte typeValue() noexcept { return singleByteOp(Operation::TypeValue); }
+    constexpr byte load() noexcept { return singleByteOp(Operation::Load); }
+    constexpr byte store() noexcept { return singleByteOp(Operation::Load); }
+    constexpr byte pow() noexcept { return singleByteOp(Operation::Pow); }
+    constexpr QuarterAddress move(TargetRegister dest, TargetRegister src) noexcept { return encodeTwoByte(Operation::Move, dest, src); }
+    constexpr QuarterAddress swap(TargetRegister dest, TargetRegister src) noexcept { return encodeTwoByte(Operation::Swap, dest, src); }
+    constexpr byte popA() noexcept { return singleByteOp(Operation::PopA); }
+    constexpr byte popB() noexcept { return singleByteOp(Operation::PopB); }
+    constexpr byte popT() noexcept { return singleByteOp(Operation::PopT); }
+    constexpr byte pushC() noexcept { return singleByteOp(Operation::PushC); }
+    constexpr QuarterAddress encodeQuarterAddress(byte a = 0, byte b = 0) noexcept {
+        return encodeBits<QuarterAddress, byte, 0xFF00, 8>(static_cast<QuarterAddress>(a), b);
+    }
+    constexpr HalfAddress encodeHalfAddress(byte a = 0, byte b = 0, byte c = 0, byte d = 0) noexcept {
+        return encodeBits<HalfAddress, QuarterAddress, 0xFFFF0000, 16>(
+                static_cast<HalfAddress>(encodeQuarterAddress(a, b)),
+                encodeQuarterAddress(c, d));
+    }
+    constexpr Address encodeOperation(byte a = 0, byte b = 0, byte c = 0, byte d = 0, byte e = 0, byte f = 0, byte g = 0, byte h = 0) {
+        return encodeBits<Address, HalfAddress, 0xFFFFFFFF00000000, 32>( static_cast<Address>(encodeHalfAddress(a, b, c, d)), encodeHalfAddress(e, f, g, h));
+    }
 } // end namespace Instruction
 constexpr bool legalOperation(Operation op) noexcept {
     return static_cast<byte>(Operation::Count) <= static_cast<byte>(op);

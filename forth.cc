e57465655ef19a1a45e14542f131f785599ebbca
Josh Scoggins <jscoggins@parasoft.com>
2018-02-01 06:25:47 -0800

    Reduce number of includes in forth.cc

--- forth.cc
+++ forth.cc
@@ -1,16 +1,4 @@
-// Currently on chapter 4.4.4
-// TODO: support nested if statements
 #include <iostream>
-#include <string>
-#include <list>
-#include <memory>
-#include <limits>
-#include <sstream>
-#include <cmath>
-#include "Types.h"
-#include "Problem.h"
-#include "Datum.h"
-#include "DictionaryEntry.h"
 #include "Machine.h"
 
 

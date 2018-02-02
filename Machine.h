5946fcf9944b2f55129a934ff49bea69e4d7d982
Josh Scoggins <jscoggins@parasoft.com>
2018-02-02 15:00:32 -0800

    further code cleanup

--- Machine.h
+++ Machine.h
@@ -165,18 +165,20 @@ namespace forth {
                     moleculeWord<rest...>();
                 }
             }
+            template<auto first, auto ... rest>
+            void singleMoleculeSequence() {
+                moleculeWord<Instruction::encodeOperation(first, std::move(rest)...)>();
+            }
 
             template<auto first, auto ... rest>
             void moleculeSequence() {
                 // we could encode each operation into a separate word to start with
-                moleculeWord<static_cast<Address>(first)>();
-                // there is no compaction in this method unless we turn out to 
-                // already have a packed address
-                moleculeSequence<rest...>();
-            }
-            template<auto first, auto ... rest>
-            void singleMoleculeSequence() {
-                moleculeWord<Instruction::encodeOperation(first, std::move(rest)...)>();
+                singleMoleculeSequence<first>();
+                if constexpr (sizeof...(rest) > 0) {
+                    // there is no compaction in this method unless we turn out to 
+                    // already have a packed address
+                    moleculeSequence<rest...>();
+                }
             }
 		private:
 			// define the CPU that the forth interpreter sits on top of

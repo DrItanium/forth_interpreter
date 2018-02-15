fe3c993e54b0e0bfd3727295b4fc5aef3eb87dba
Josh Scoggins <jscoggins@parasoft.com>
2018-02-15 06:10:18 -0800

    Fixed the load operation

--- Core.cc
+++ Core.cc
@@ -653,7 +653,7 @@ void Core::loadStore(Operation op) {
 		 auto trs = TargetRegister(getSourceRegister(k));
 		 auto& dest = getRegister(trd);
 		 auto& src = getRegister(trs);
-         dest.setValue(src.getAddress());
+         dest.setValue(load(src.getAddress()));
 	} else if (op == Operation::Store) {
 		 auto k = extractByteFromMolecule();
 		 auto trd = TargetRegister(getDestinationRegister(k));
@@ -718,6 +718,8 @@ template<Immediate16Positions pos>
 constexpr auto Immediate16ShiftIndex = static_cast<Address>(pos) * 16;
 template<Immediate16Positions pos>
 constexpr auto Immediate16Mask = static_cast<Address>(0xFFFF) << Immediate16ShiftIndex<pos>;
+template<>
+constexpr auto Immediate16Mask<Immediate16Positions::Lowest> = static_cast<Address>(0xFFFF);
 
 template<Immediate16Positions pos>
 constexpr Address computeImmediate16(Address base, QuarterAddress value) noexcept {

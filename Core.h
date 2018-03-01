1b31fdd7954fea39c97ec6382a5f7e9d6433769b
Josh Scoggins <jscoggins@parasoft.com>
2018-03-01 13:09:07 -0800

    Make sure that NoArguments is first in the std::variant
    - It will become the default if you don't assign or init with a specific value

--- Core.h
+++ Core.h
@@ -120,7 +120,7 @@ class Core {
         };
         using EightByteVariant = std::variant<IsLoadImm48>;
 #undef OperationKind
-        using DecodedArguments = std::variant<OneRegister, TwoRegister, FourRegister, FiveRegister , ThreeRegister, QuarterInteger, IsImm24, TwoRegisterWithImm16, OneRegisterWithImm16, LoadImm48, Imm24, SignedImm16, NoArguments>;
+        using DecodedArguments = std::variant<NoArguments, OneRegister, TwoRegister, FourRegister, FiveRegister , ThreeRegister, QuarterInteger, IsImm24, TwoRegisterWithImm16, OneRegisterWithImm16, LoadImm48, Imm24, SignedImm16>;
         using DecodedInstruction = std::tuple<Operation, DecodedArguments>;
     public:
         static OneByteVariant getVariant(Operation op, const OneByte&);

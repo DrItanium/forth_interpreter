02799047708a53826fdd66f357076a94e5209539
Josh Scoggins <jscoggins@parasoft.com>
2018-03-23 18:14:39 -0700

    Migrate the store and load routines into the forth library

--- cpp/basics.fs
+++ cpp/basics.fs
@@ -29,7 +29,6 @@
 : u<= ( u1 u2 -- f ) <=.u ;
 : u> ( u1 u2 -- f ) >.u ;
 : u>= ( u1 u2 -- f ) >=.u ;
-
 : 1+ ( n1 -- n2 ) 1 + ;
 : 1- ( n1 -- n2 ) 1 - ;
 : 2+ ( n1 -- n2 ) 2 + ;
@@ -37,6 +36,7 @@
 : 2* ( n1 -- n2 ) 2 * ;
 : 2/ ( n1 -- n2 ) 2 / ;
 : 4+ ( n1 -- n2 ) 4 + ;
+: 4/ ( n1 -- n2 ) 4 / ;
 : nop ( -- ) ;
 : and ( n1 n2 -- n3 ) &&.b ;
 : or ( n1 n2 -- n3 ) ||.b ;
@@ -51,16 +51,76 @@
 : 0<> ( n -- f ) 0 <> ;
 : 0=  ( n -- f ) 0 = ;
 ( TODO: add support for within ! )
-
+: << ( n c -- v ) <<.s ;
+: <<u ( n c -- v ) <<.u ;
+: >> ( n c -- v )  >>.s ;
+: >>u ( n c -- v ) >>.u ;
 : under+ ( n1 n2 n3 -- n n2 ) rot + swap ;
 : min ( n1 n2 -- n ) 2dup > if swap then drop ;
 : max ( n1 n2 -- n ) 2dup < if swap then drop ;
 : negate ( n1 -- n2 ) minus.s ;
-
+: *sizeof-byte* ( -- 1 ) 1 ;
+: bitcount ( n -- v ) *bitwidth* * ;
 : quit ( -- ) bye ;
 
+: half-of ( n -- v ) 2/ ;
+: quarter-of ( n -- v ) 4/ ;
+: *address-plus-offset* ( addr offset -- addr+offset addr )
+    over ( addr offset -- addr offset addr )
+    +    ( addr offset -- addr offset+addr )
+    swap ( addr offset+addr -- offset+addr addr ) ;
+: *shift-left-then-bitwise-or* ( a b c -- n )
+  <<u ( a b c -- a d )
+  |.u ( a d -- n ) ;
+( TODO take advantage of constants to get rid of magic constants )
+: mload.quarter ( addr -- value )
+  1 *address-plus-offset*
+  mload.byte
+  swap
+  mload.byte
+  8 *shift-left-then-bitwise-or* ;
 
-: of-type? ( type code -- flag ) swap type-code ==.u ;
+: mload.half ( addr -- value )
+  2 *address-plus-offset*
+  mload.quarter
+  swap
+  mload.quarter
+  16 *shift-left-then-bitwise-or* ;
+
+: mload.full ( addr -- value )
+  4 *address-plus-offset*
+  mload.half 
+  swap
+  mload.half
+  32 *shift-left-then-bitwise-or* ;
+
+: mstore.quarter ( addr value -- )
+    2dup 
+    mstore.byte 
+    8 >>u
+    swap
+    1+
+    swap
+    mstore.byte ;
+
+: mstore.half ( addr value -- )
+  2dup
+  mstore.quarter
+  16 >>u 
+  swap
+  2+
+  swap
+  mstore.quarter ;
+
+: mstore.full ( addr value -- )
+  2dup
+  mstore.half
+  32 >>u
+  swap
+  4+
+  mstore.half ;
+
+: of-type? ( type code -- flag ) swap type-code = ;
 : number? ( a -- flag ) *number-variant-code* of-type? ;
 : word? ( a -- flag ) *word-variant-code* of-type? ;
 : native-function? ( a -- flag ) *native-function-variant-code* of-type? ;
--- cpp/main.cc
+++ cpp/main.cc
@@ -727,86 +727,6 @@ template<typename T>
 Number minusOperation(Number a) {
     return Number(- a.get<T>());
 }
-void shiftLeftByConstantThenAnd(Machine& m, Address shift) {
-    // ( a -- b ) 
-    m.pushParameter(Number(shift));
-    invokeBinaryNumberOperation(m, shiftLeft<Address>); 
-    invokeBinaryNumberOperation(m, bitwiseOr<Address>);
-}
-void addressPlusOffset(Machine& m, Address size) {
-    // ( a -- b a )
-    dup(m);
-    m.pushParameter(Number(size));
-    invokeBinaryNumberOperation(m, add<Address>);
-    swap(m);
-}
-void loadQuarter(Machine& m) {
-    addressPlusOffset(m, 1);
-    loadByte(m);
-    swap(m);
-    loadByte(m);
-    shiftLeftByConstantThenAnd(m, 8);
-}
-
-void loadHalf(Machine& m) {
-    addressPlusOffset(m, 2);
-    loadQuarter(m);
-    swap(m);
-    loadQuarter(m);
-    shiftLeftByConstantThenAnd(m, 16);
-}
-void loadFull(Machine& m) {
-    addressPlusOffset(m, 4);
-    loadHalf(m);
-    swap(m);
-    loadHalf(m);
-    shiftLeftByConstantThenAnd(m, 32);
-}
-
-void storeQuarter(Machine& m) {
-    // ( addr value -- )
-    over(m); // ( addr value -- addr value addr )
-    over(m); // ( addr value addr -- addr value addr value )
-    storeByte(m); // ( addr value addr value -- addr value )
-                  // get the upper 8 bits to store
-    m.pushParameter(Number(8));
-    invokeBinaryNumberOperation(m, shiftRight<Address>);
-    swap(m); // ( addr value -- value addr )
-    m.pushParameter(Number(1)); // ( addr value -- value addr 1 )
-    invokeBinaryNumberOperation(m, add<Address>); // ( value addr 1 -- value addr* )
-    swap(m);
-    storeByte(m); // ( addr* value -- )
-}
-
-void storeHalf(Machine& m) {
-    // ( addr value -- )
-    over(m); // ( addr value -- addr value addr )
-    over(m); // ( addr value addr -- addr value addr value )
-    storeQuarter(m); // ( addr value addr value -- addr value )
-                  // get the upper 16 bits to store
-    m.pushParameter(Number(16));
-    invokeBinaryNumberOperation(m, shiftRight<Address>);
-    swap(m); // ( addr value -- value addr )
-    m.pushParameter(Number(2)); // ( addr value -- value addr 1 )
-    invokeBinaryNumberOperation(m, add<Address>); // ( value addr 1 -- value addr* )
-    swap(m);
-    storeQuarter(m); // ( addr* value -- )
-}
-
-void storeFull(Machine& m) {
-    // ( addr value -- )
-    over(m); // ( addr value -- addr value addr )
-    over(m); // ( addr value addr -- addr value addr value )
-    storeHalf(m); // ( addr value addr value -- addr value )
-                  // get the upper 16 bits to store
-    m.pushParameter(Number(32));
-    invokeBinaryNumberOperation(m, shiftRight<Address>);
-    swap(m); // ( addr value -- value addr )
-    m.pushParameter(Number(4)); // ( addr value -- value addr 1 )
-    invokeBinaryNumberOperation(m, add<Address>); // ( value addr 1 -- value addr* )
-    swap(m);
-    storeHalf(m); // ( addr* value -- )
-}
 
 void setupDictionary(Machine& mach) {
 	mach.addWord("words", words);
@@ -821,13 +741,7 @@ void setupDictionary(Machine& mach) {
     mach.addWord(";", semicolon, false, true);
     mach.addWord("bye", bye);
     mach.addWord("mload.byte", loadByte);
-    mach.addWord("mload.quarter", loadQuarter);
-    mach.addWord("mload.half", loadHalf);
-    mach.addWord("mload.full", loadFull);
     mach.addWord("mstore.byte", storeByte);
-    mach.addWord("mstore.quarter", storeQuarter);
-    mach.addWord("mstore.half", storeHalf);
-    mach.addWord("mstore.full", storeFull);
     mach.addWord(".", printTop);
     mach.addWord(":", enterCompileMode);
     mach.addWord(".s", std::mem_fn(&Machine::viewParameterStack));
@@ -864,6 +778,10 @@ void setupDictionary(Machine& mach) {
 #define Y(name, op, type) mach.addWord(#op INDIRECTION(YTypeString, type) , callBinaryNumberOperation( name < INDIRECTION(YType, type) >));
 #include "BinaryOperators.def"
 #undef X
+    addConstantWord(mach, "sizeof-address", sizeof(Address));
+    addConstantWord(mach, "sizeof-half-address", sizeof(forth::HalfAddress));
+    addConstantWord(mach, "sizeof-quarter-address", sizeof(forth::QuarterAddress));
+    addConstantWord(mach, "bitwidth", CHAR_BIT);
 }
 int main() {
     Machine mach;

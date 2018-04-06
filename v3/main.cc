c601e15b75403a39cd31867987c8d16423f15f60
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-04-05 18:42:43 -0700

    Added more routines

--- v3/main.cc
+++ v3/main.cc
@@ -19,20 +19,21 @@
 #include <cstdint>
 #include <functional>
 #include <string>
+#include <sstream>
 
 using byte = uint8_t;
-using Integer = int64_t;
-
-using Number = Integer;
+using Number = int64_t;
+using Address = uint64_t;
 
 constexpr bool getTruth(Number number) noexcept {
 	return number != 0;
 }
-
+constexpr auto defaultCapacity = 0x10000;
+Address capacity = defaultCapacity;
 byte* memory = nullptr;
-byte* parameterStart = nullptr;
-byte* subroutineStart = nullptr;
-byte* dictionaryStart = nullptr;
+byte* parameter = nullptr;
+byte* subroutine = nullptr;
+byte* dictionary = nullptr;
 byte* variables = nullptr;
 byte* dictionaryFront = nullptr;
 byte* systemVariables = nullptr;
@@ -44,8 +45,136 @@ void raiseError(const std::string& msg) {
 	_output = msg;
 	systemVariables[0] = 0xFF;
 }
-bool errorHappened() noexcept {
-	return systemVariables[0] != 0;
+bool inRange(Address addr) noexcept {
+	return addr < defaultCapacity;
 }
+void writeMemory(Address addr, byte value) noexcept {
+	if (!inRange(addr)) {
+		raiseError("!bad address");
+		return;
+	} else {
+		memory[addr] = value;
+	}
+}
+byte readMemory(Address addr) noexcept {
+	if (!inRange(addr)) {
+		raiseError("!bad address");
+		return 0;
+	} else {
+		return memory[addr];
+	}
+}
+
+bool readFlag(Address addr) noexcept {
+	return readMemory(addr) != 0;
+}
+Address readNumber16(Address addr) noexcept {
+	auto lower = readMemory(addr);
+	auto upper = readMemory(addr + 1);
+	return Address(lower) | (Address(upper) << 8);
+}
+Address readNumber32(Address addr) noexcept {
+	auto lower = readNumber16(addr);
+	auto upper = readNumber16(addr + (sizeof(Address) / 4));
+	return lower | (upper << 16);
+}
+
+Address readAddress(Address addr) noexcept {
+	auto lower = readNumber32(addr);
+	auto upper = readNumber32(addr + (sizeof(Address) / 2));
+	return lower | (upper << 32);
+}
+Address systemVariable(Address offset) noexcept {
+	return (systemVariables - memory) + offset;
+}
+
+bool errorHappened() noexcept {
+	return readFlag(systemVariable(0));
+}
+bool keepExecuting() noexcept {
+	return readFlag(systemVariable(1));
+}
+bool isCompiling() noexcept {
+	return readFlag(systemVariable(2));
+}
+Address parameterBottom() noexcept {
+	return readAddress(systemVariable(8));
+}
+Address parameterTop() noexcept {
+	return readAddress(systemVariable(16));
+}
+byte getDictionaryControlByte(Address addr) {
+	return readMemory(addr);
+}
+byte getDictionaryNameLength(Address addr) {
+	auto result = getDictionaryControlByte(addr);
+	return result & 0b00011111;
+}
+bool entryIsFake(Address addr) {
+	auto result = getDictionaryControlByte(addr);
+	return (result & 0b00100000) != 0;
+}
+bool invokeAtCompileTime(Address addr) {
+	auto result = getDictionaryControlByte(addr);
+	return (result & 0b01000000) != 0;
+}
+char* getDictionaryStringStart(Address addr) {
+	if (!inRange(addr)) {
+		raiseError("!bad address");
+		return nullptr;
+	} else {
+		return (char*)(&memory[addr + 1]);
+	}
+}
+void loadDictionaryName(Address addr) {
+	auto length = getDictionaryNameLength(addr);
+	if (!errorHappened()) {
+		auto str = getDictionaryStringStart(addr);
+		_output.assign(str, length);
+	}
+}
+void pushParameter(Number n) {
+
+}
+void readNext() noexcept {
+	std::cin >> _input;
+}
+void number() noexcept {
+	if (_input[0] == '0' && _input[1] == 'x') {
+		auto copy = _input;
+		copy[1] = 0;
+		std::istringstream input(copy);
+		Number n;
+		input >> std::hex >> n;
+		if (!input.fail() && input.eof()) {
+			pushParameter(n);
+			return;
+		}
+	} else {
+		Number n;
+		std::istringstream input(_input);
+		input >> n;
+		if (!input.fail() && input.eof()) {
+			pushParameter(n);
+			return;
+		}
+	}
+
+	std::stringstream ss;
+	ss << _input << "?";
+	auto str = ss.str();
+	raiseError(str);
+}
+
+void controlLoop() {
+	while (keepExecuting()) {
+
+		if (errorHappened()) {
+			std::cout << _output << std::endl;
+
+		}
+	}
+}
+
 
 
